d0b64c3d09ac0a60827e07f17ce9974286eaf281
misterbahari <misterbaharidb@gmail.com>
2023-11-29 19:01:17 +0330

    session01

---
+++ session01/file02.py
@@ -0,0 +1,23 @@
+name1 = (input())
+
+name = '   kiloo kiloo   h   '
+print(name)
+
+print(name.strip())
+print(name.upper())
+
+print(name.lower())
+print(name.lstrip())
+print(name.rstrip())
+
+print(name1.count('v'))
+
+
+
+
+
+
+
+
+
+
---
+++ session01/file03.py
@@ -0,0 +1,9 @@
+#name = input('esm bede')
+#print(name)
+
+#num = input('esm bede ba "_"')
+#print(num.split('_'))
+
+print("foobarfoobarfoobarfoobarfoobar"[0:12:2])
+
+
