410f31457231806333fcb1c0beb0cd4dce9f6d43
misterbahari <misterbaharidb@gmail.com>
2023-12-20 19:05:58 +0330

    session04

---
+++ session04/file13.py
@@ -0,0 +1,26 @@
+var_set = {2,3,65,'ali',True,'kiloo'}
+set2 = {2,65,76,'reza',False}
+print(var_set)
+#print(type(len(var_set)))
+#print(2 in var_set)
+#print(var_set.clear())
+#print(var_set)
+#print(var_set.copy())
+
+#print(var_set.add('mohammad'))
+#print(var_set)
+
+#print(var_set.pop())
+#print(var_set)
+
+#print(var_set.discard('reza'))
+#print(var_set)
+
+#print(var_set.remove('ali'))
+#print(var_set)                   ######read about the differences between these two
+
+print(var_set.difference(set2))
+
+
+
+
---
+++ session04/file14.py
@@ -0,0 +1,32 @@
+var_dict = {
+    'name': 'mmreza',
+    'family': "rafiei",
+    'age': 21,
+    'phone': '0913',
+    'is_single': True
+}
+
+#print(var_dict.pop('family'))
+#print(var_dict.popitem())
+#print((var_dict.items()))
+print(var_dict.get('skin','yellow'))
+#print(var_dict.get('age'))
+
+print(var_dict.setdefault('skin','yellow'))
+
+print(var_dict)
+#print(type(var_dict))
+
+#print(var_dict[0])    #wrong
+
+#print(var_dict.keys())
+#print(var_dict.values())
+
+
+
+#print(type(var_dict.keys()))
+#print(type(var_dict.values()))
+
+
+
+
