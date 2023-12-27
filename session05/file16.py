efaf958a5260121eeec4d4de08f401c70d81cdef
misterbahari <misterbaharidb@gmail.com>
2023-12-27 19:05:36 +0330

    session05

---
+++ session05/file16.py
@@ -0,0 +1,17 @@
+rainy = int(input('baaroon miad ya na? ba 0 ya 1'))
+if rainy == 1:
+    chatr = int(input('chatr dari ya na? ba 0 ya 1'))
+if rainy == 1 and chatr == 1:
+    print('boro')
+else:
+    print('naro')
+#######################################
+if 8 == 8:
+    pass
+
+#######################################
+if 5 > 3:
+    print('salam')
+elif 9 > 5:
+    print('bye bye')
+#######################################
---
+++ session05/file17.py
@@ -0,0 +1,10 @@
+rainy = int(input('0 ya 1'))
+while rainy < 3:
+    print('boro biroon')
+    if rainy != 1:
+        print('naro dada')
+    rainy += 1
+    print(rainy)
+
+
+print('salam')
